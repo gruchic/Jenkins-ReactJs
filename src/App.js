f9e1929e174eba0261a0c02f8ee5ac436fd6861d
gru <gurpreet.singh@chicmicstudios.in>
2025-03-26 10:30:53 +0530

    Email Checking

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Js
+          Learn React Js with Jenkins Now
         </a>
       </header>
     </div>
